caad3e0e1858aaef5bd0f902d5686f604c5feba7
Derek Tan <derektanbusiness@gmail.com>
2020-07-13 12:15:41 -0400

    created scraper file

---
+++ execOrders.js
@@ -0,0 +1,5 @@
+// scrapes all executive orders signed and executed by President Donald Trump
+
+const rp = require('request-promise');
+const url = 'https://en.wikipedia.org/wiki/List_of_executive_actions_by_Donald_Trump';
+
